1e762b947fc7567016559e2c4d97df8baa422647
MatthMiller <matheus.monteiro.genesis@gmail.com>
2023-10-20 17:58:01 -0300

    :memo: Etapa 3 - documentação

--- src/login/User.java
+++ src/login/User.java
@@ -5,52 +5,69 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/** A classe User é responsável por estabelecer uma conexão com o banco de dados,
+ * autenticar o login do usuário e armazenar o nome do usuário.
+ */
 public class User {
-    public Connection conectarBD(){ // 2. Tenta conectar ao banco de dados
+    /** Método conectarBD: 
+     * Cria uma conexão com o banco de dados.
+     * @return Retorna a conexão para ser usada em outros métodos. 
+     */
+    public Connection conectarBD(){
         Connection conn = null;
         try{
             Class.forName("com.mysql.Driver.Manager").newInstance();
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
             conn =  DriverManager.getConnection(url);
         }catch (Exception e) {
-        
+            // Trata exceções, se ocorrerem
         }
         return conn;
     }
-    // 03. Retorno da conexão
-    // 05. Sucesso ao conectar. Continua para o 6, ou
-    // 04. Falha da conexão
-    // 13. Fim do programa (erro)
 
-    // 01. Declaração de varíaveis/propriedades
+    /* As variaveis da classe são declaradas e inicializadas. */
     public String nome="";
+     /** A variável 'nome' armazenará o nome do usuário,
+     * caso o login seja bem-sucedido.
+     */
     public boolean result = false;
     
+    /** 
+     * A variável 'result' indica se o usuário fez o login com sucesso ou não.
+     * Se o login for bem-sucedido, o valor é alterado para True.
+     */
+    
+    /** 
+     * Método verificarUsuario: 
+     * Executa uma consulta MYSQL com o login e senha fornecidos pelo usuário,
+     * determinando se o login foi bem-sucedido ou não.
+     * @return Retorna um valor booleano que indica se o login foi bem-sucedido ou não.
+     */
     public boolean verificarUsuario(String login, String senha){
-        // 06. Consulta no banco de dados
         String sql = "";
         Connection conn = conectarBD();
-        //INSTRUÇÃO SQL
+        // INSTRUÇÃO SQL
+        /** 
+         * Concatena uma String predefinida com o login e senha inseridos pelo usuário,
+         * gerando a instrução SQL que realizará a consulta.
+         */
         sql += "select nome from usuarios ";
         sql +="where login = " + "'" + login + "'";
         sql += " and senha = " + "'" + senha + "';";
         try {
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            // 07. Retorno da consulta
-            
-            // 08. Validação do retorno
-            // 10. Consulta com restultados. Continua para o 11
-            // 09. Consulta sem resultados e
-            // 13. Fim do programa (erro)
+            /** 
+             * Verifica se houve um resultado da consulta realizada.
+             * Se não houver, uma exceção é lançada.
+             * Se houver, o código continua, e as variáveis 'result' e 'nome' recebem novos valores.
+             */
             if(rs.next()){
-                // 11. Variáveis recebem novos valores
                 result = true;
                 nome = rs.getString("nome");}
-            } catch (Exception e) { } 
-                // 12. Propriedade "result" é retornada pelo método
-                // 13. Fim do programa. (erro)
+            } catch (Exception e) {
+                // Trata exceções, se ocorrerem
+            } 
                 return result;
             } 
-            // 13. Fim do programa (sucesso)
         }
